0f63a53db00eb499056a0ab818b42b248c77b710
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-22 10:52:53 -0500

    implimented baubles api :D

---
+++ src/main/java/com/ukminecrafted/mitrek/common/items/WeaponBase.java
@@ -0,0 +1,9 @@
+package com.ukminecrafted.mitrek.common.items;
+
+import net.minecraft.item.ItemSword;
+
+public class WeaponBase extends ItemSword {
+    public WeaponBase(ToolMaterial material) {
+        super(material);
+    }
+}
