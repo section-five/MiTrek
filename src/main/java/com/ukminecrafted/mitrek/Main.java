b0e6abdeaa510b732f5b9d3025031905018e37d0
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-22 11:00:45 -0500

    attempted to add jei, need to reimport thingws

--- src/main/java/com/ukminecrafted/mitrek/Main.java
+++ src/main/java/com/ukminecrafted/mitrek/Main.java
@@ -19,8 +19,8 @@ public class Main
     public static final String MODID = "mitrek";
     public static final String NAME = "MiTrek";
     public static final String VERSION = "1.0";
-    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2768,)";
-    public static final String UPDATEURL = "https://raw.githubusercontent.com/bpteammc/SeasonalHeros/master/update.json";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2813,)";
+    public static final String UPDATEURL = "https://raw.githubusercontent.com/ukminecrafted/MiTrek/master/update.json";
 
 
     @SidedProxy(clientSide = Reference.CLIENTPROXY, serverSide = Reference.COMMONPROXY)
