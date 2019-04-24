760da5ea72a90e88090583883294b68c11552d78
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-23 20:25:15 -0500

    fixed batleth, forgot to register model

--- src/main/java/com/ukminecrafted/mitrek/common/items/WeaponBase.java
+++ src/main/java/com/ukminecrafted/mitrek/common/items/WeaponBase.java
@@ -4,23 +4,21 @@ import com.ukminecrafted.mitrek.Main;
 import com.ukminecrafted.mitrek.init.ModItems;
 import com.ukminecrafted.mitrek.util.IHasModel;
 import net.minecraft.item.ItemSword;
-import net.minecraft.item.ItemTool;
 
-import javax.tools.Tool;
 
 public class WeaponBase extends ItemSword implements IHasModel
-    {
+{
         public WeaponBase(String name,ToolMaterial material) {
             super(material);
         setTranslationKey(name);
         setRegistryName(name);
         setCreativeTab(Main.items);
-
         ModItems.ITEMS.add(this);
     }
 
-    @Override
-    public void registerModels() {
+        @Override
+        public void registerModels() {
+            Main.proxy.registerItemRenderer(this, 0, "inventory");
+        }
 
-    }
 }
