55223cef0d3c488df85de16d1f6a8dfc131d7beb
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-25 19:27:25 -0500

    combadge now is a bauble

--- src/main/java/com/ukminecrafted/mitrek/common/items/ItemCombadge.java
+++ src/main/java/com/ukminecrafted/mitrek/common/items/ItemCombadge.java
@@ -1,11 +1,16 @@
 package com.ukminecrafted.mitrek.common.items;
 
+import baubles.api.BaubleType;
+import baubles.api.IBauble;
+import baubles.api.cap.BaublesCapabilities;
 import com.ukminecrafted.mitrek.Main;
 import com.ukminecrafted.mitrek.init.ModItems;
 import com.ukminecrafted.mitrek.util.IHasModel;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 
-public class ItemCombadge extends Item implements IHasModel {
+public class ItemCombadge extends Item implements IHasModel, IBauble {
     public ItemCombadge(String name) {
         setUnlocalizedName(name);
         setRegistryName(name);
@@ -13,9 +18,17 @@ public class ItemCombadge extends Item implements IHasModel {
 
         ModItems.ITEMS.add(this);
     }
+
+
+
+
     @Override
     public void registerModels() {
         Main.proxy.registerItemRenderer(this, 0, "inventory");
     }
 
+    @Override
+    public BaubleType getBaubleType(ItemStack itemstack) {
+        return BaubleType.BODY;
+    }
 }
