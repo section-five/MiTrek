909b8d3fa51c8183be42e3fb506e81f821d2e0fd
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-28 07:48:37 -0500

    added tricorder
    tryed something didn't work disabled for now

--- src/main/java/com/ukminecrafted/mitrek/init/ModItems.java
+++ src/main/java/com/ukminecrafted/mitrek/init/ModItems.java
@@ -1,9 +1,6 @@
 package com.ukminecrafted.mitrek.init;
 
-import com.ukminecrafted.mitrek.common.items.ArmorBase;
-import com.ukminecrafted.mitrek.common.items.ItemBase;
-import com.ukminecrafted.mitrek.common.items.ItemCombadge;
-import com.ukminecrafted.mitrek.common.items.WeaponBase;
+import com.ukminecrafted.mitrek.common.items.*;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 
@@ -18,6 +15,7 @@ public class ModItems {
     //item
     public static final Item COMBADGE = new ItemCombadge("combadge");
     public static final Item BATLETH = new WeaponBase("batleth", ModMaterials.BATLETH);
+    public static final Item TRICORDER = new ItemTricorder("tricorder");
 
     //Voyager Command
     public static final Item VOY_COMMAND_SHIRT = new ArmorBase("voy_command_shirt", VOY_COMMAND_UNIFORM, 1, EntityEquipmentSlot.CHEST);
@@ -33,4 +31,19 @@ public class ModItems {
     public static final Item VOY_MEDICAL_SHIRT = new ArmorBase("voy_medical_shirt", VOY_MEDICAL_UNIFORM, 1, EntityEquipmentSlot.CHEST);
     public static final Item VOY_MEDICAL_PANTS = new ArmorBase("voy_medical_pants", VOY_MEDICAL_UNIFORM, 2, EntityEquipmentSlot.LEGS);
     public static final Item VOY_MEDICAL_BOOTS = new ArmorBase("voy_medical_boots", VOY_MEDICAL_UNIFORM, 1, EntityEquipmentSlot.FEET);
+
+    //TNG Command
+    public static final Item TNG_COMMAND_SHIRT = new ArmorBase("tng_command_shirt", TNG_COMMAND_UNIFORM, 1, EntityEquipmentSlot.CHEST);
+    public static final Item TNG_COMMAND_PANTS = new ArmorBase("tng_command_pants", TNG_COMMAND_UNIFORM, 2, EntityEquipmentSlot.LEGS);
+    public static final Item TNG_COMMAND_BOOTS = new ArmorBase("tng_command_boots", TNG_COMMAND_UNIFORM, 1, EntityEquipmentSlot.FEET);
+
+    //TNG Engineering
+    public static final Item TNG_ENGINEERING_SHIRT = new ArmorBase("tng_engineering_shirt", TNG_ENGINEERING_UNIFORM, 1, EntityEquipmentSlot.CHEST);
+    public static final Item TNG_ENGINEERING_PANTS = new ArmorBase("tng_engineering_pants", TNG_ENGINEERING_UNIFORM, 2, EntityEquipmentSlot.LEGS);
+    public static final Item TNG_ENGINEERING_BOOTS = new ArmorBase("tng_engineering_boots", TNG_ENGINEERING_UNIFORM, 1, EntityEquipmentSlot.FEET);
+
+    //TNG Medical
+    public static final Item TNG_MEDICAL_SHIRT = new ArmorBase("tng_medical_shirt", TNG_MEDICAL_UNIFORM, 1, EntityEquipmentSlot.CHEST);
+    public static final Item TNG_MEDICAL_PANTS = new ArmorBase("tng_medical_pants", TNG_MEDICAL_UNIFORM, 2, EntityEquipmentSlot.LEGS);
+    public static final Item TNG_MEDICAL_BOOTS = new ArmorBase("tng_medical_boots", TNG_MEDICAL_UNIFORM, 1, EntityEquipmentSlot.FEET);
 }
