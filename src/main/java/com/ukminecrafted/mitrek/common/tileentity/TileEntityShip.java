d927c3869a7d27534c5b5f64e6cb0a70256170ec
Josia50 <elwyn.vdb@gmail.com>
2019-04-30 19:55:46 +0200

    Ship Tile

---
+++ src/main/java/com/ukminecrafted/mitrek/common/tileentity/TileEntityShip.java
@@ -0,0 +1,25 @@
+package com.ukminecrafted.mitrek.common.tileentity;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+public class TileEntityShip extends TileEntity implements ITickable {
+
+    private boolean warping = false;
+    private boolean isFlying = false;
+
+    @Override
+    public void update() {}
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        return compound;
+    }
+}
