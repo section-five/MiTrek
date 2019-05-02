bfa9a3f6cc119b0003547d6963523e8c198f6aad
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-05-02 13:57:22 -0500

    alphabet letter hull registry blocks

--- src/main/java/com/ukminecrafted/mitrek/init/ModBlocks.java
+++ src/main/java/com/ukminecrafted/mitrek/init/ModBlocks.java
@@ -8,5 +8,33 @@ import java.util.List;
 public class ModBlocks {
 
     public static final List<Block> BLOCKS = new ArrayList<Block>();
-    //public static final Block ICY_SNOW = new ABlockIcySnow("icy_snow");
+    public static final Block STARSHIP_BLOCK = new BlockBase("starship_hull");
+    public static final Block A_BLOCK = new BlockBase("a_block");
+    public static final Block B_BLOCK = new BlockBase("b_block");
+    public static final Block C_BLOCK = new BlockBase("c_block");
+    public static final Block D_BLOCK = new BlockBase("d_block");
+    public static final Block E_BLOCK = new BlockBase("e_block");
+    public static final Block F_BLOCK = new BlockBase("f_block");
+    public static final Block G_BLOCK = new BlockBase("g_block");
+    public static final Block H_BLOCK = new BlockBase("h_block");
+    public static final Block I_BLOCK = new BlockBase("i_block");
+    public static final Block J_BLOCK = new BlockBase("j_block");
+    public static final Block J_BLOCK = new BlockBase("k_block");
+    public static final Block L_BLOCK = new BlockBase("l_block");
+    public static final Block M_BLOCK = new BlockBase("m_block");
+    public static final Block N_BLOCK = new BlockBase("n_block");
+    public static final Block O_BLOCK = new BlockBase("o_block");
+    public static final Block P_BLOCK = new BlockBase("p_block");
+    public static final Block Q_BLOCK = new BlockBase("q_block");
+    public static final Block R_BLOCK = new BlockBase("r_block");
+    public static final Block S_BLOCK = new BlockBase("s_block");
+    public static final Block T_BLOCK = new BlockBase("t_block");
+    public static final Block U_BLOCK = new BlockBase("u_block");
+    public static final Block V_BLOCK = new BlockBase("v_block");
+    public static final Block W_BLOCK = new BlockBase("w_block");
+    public static final Block X_BLOCK = new BlockBase("x_block");
+    public static final Block Y_BLOCK = new BlockBase("y_block");
+    public static final Block Z_BLOCK = new BlockBase("z_block");
+  
+    
 }
