77e4b5cdc6fcb61df2303db0a289c058d2bebb81
Josia50 <elwyn.vdb@gmail.com>
2019-05-19 21:04:48 +0200

    Ship Exterior

--- src/main/java/com/bpteammc/mitrek/client/renderers/RenderShipExterior_01.java
+++ src/main/java/com/bpteammc/mitrek/client/renderers/RenderShipExterior_01.java
@@ -1,19 +1,32 @@
 package com.bpteammc.mitrek.client.renderers;
 
+import com.bpteammc.mitrek.Mitrek;
 import com.bpteammc.mitrek.client.models.EnterPriseShip;
+import com.bpteammc.mitrek.common.blocks.BlockShipExterior;
 import com.bpteammc.mitrek.common.tileentity.exteriors.TileShipExterior_01;
+import com.bpteammc.mitrek.util.helper.RenderHelpers;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.ResourceLocation;
 
 public class RenderShipExterior_01 extends TileEntitySpecialRenderer<TileShipExterior_01> {
 
     public EnterPriseShip priseShip = new EnterPriseShip();
+    public static ResourceLocation texture = new ResourceLocation(Mitrek.MODID, "tewtures/block/enterprise");
 
     @Override
     public void render(TileShipExterior_01 te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-        GlStateManager.popMatrix();
-        priseShip.render(null, 0,0,0,0,0,0.065f);
         GlStateManager.pushMatrix();
-
+        GlStateManager.translate(x + 0.5, y + 1.5, z - 0.5);
+        GlStateManager.rotate(180, 1, 0, 0);
+        IBlockState state = te.getWorld().getBlockState(te.getPos());
+        if (state.getBlock() instanceof BlockShipExterior) {
+            GlStateManager.rotate(RenderHelpers.getAngleFromFacing(state.getValue(BlockShipExterior.FACING)), 0, 1, 0);
+        }
+        Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
+        priseShip.render(null, 0,0,0,0,0,0.0625f);
+        GlStateManager.popMatrix();
     }
 }
--- src/main/java/com/bpteammc/mitrek/common/blocks/BlockShipExterior.java
+++ src/main/java/com/bpteammc/mitrek/common/blocks/BlockShipExterior.java
@@ -5,14 +5,24 @@ import com.bpteammc.mitrek.util.IHasModel;
 import com.bpteammc.mitrek.common.tileentity.TileEntityShipExterior;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class BlockShipExterior extends BlockBase implements ITileEntityProvider, IHasModel {
 
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
     public BlockShipExterior(String name, Material material) {
         super(name, material);
@@ -27,5 +37,51 @@ public class BlockShipExterior extends BlockBase implements ITileEntityProvider,
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileShipExterior_01();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.byHorizontalIndex(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getHorizontalIndex();
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
+    }
+
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state) {
+        return EnumBlockRenderType.INVISIBLE;
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
+
+    @Override
+    public boolean isNormalCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
     }
 }
