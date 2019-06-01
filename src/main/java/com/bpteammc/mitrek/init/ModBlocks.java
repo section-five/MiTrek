3a7715a3cc38bf6b8f61c685d0554059188668a5
Josia50 <elwyn.vdb@gmail.com>
2019-06-01 08:36:22 +0200

    Still fixing door

--- src/main/java/com/bpteammc/mitrek/init/ModBlocks.java
+++ src/main/java/com/bpteammc/mitrek/init/ModBlocks.java
@@ -1,6 +1,9 @@
 package com.bpteammc.mitrek.init;
 
-import com.bpteammc.mitrek.common.blocks.*;
+import com.bpteammc.mitrek.common.blocks.BlockBase;
+import com.bpteammc.mitrek.common.blocks.BlockConsole;
+import com.bpteammc.mitrek.common.blocks.BlockShipDoor;
+import com.bpteammc.mitrek.common.blocks.BlockShipExterior;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -13,7 +16,7 @@ public class ModBlocks {
 
     public static final Block SHIP_EXTERIOR = new BlockShipExterior("ship_exterior", Material.IRON);
     public static final Block SHIP_CONSOLE = new BlockConsole("ship_console", Material.IRON);
-    public static final Block SHIP_TRANSPORTER = new BlockShipTransporter("ship_transporter", Material.IRON);
+    public static final Block SHIP_DOOR = new BlockShipDoor("ship_door", Material.IRON);
     public static final Block STARSHIP_HULL = new BlockBase("starship_hull", Material.ROCK);
 
     public static final Block A_BLOCK = new BlockBase("a_block", Material.ROCK);
