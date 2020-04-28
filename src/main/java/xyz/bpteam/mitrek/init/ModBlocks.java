5f1dd8f0674c72e57121aad7f54a85f7540e58c2
ukminecrafted <ukminecrafted@gmail.com>
2020-04-28 12:14:24 -0500

    wip stuff item groups

--- src/main/java/xyz/bpteam/mitrek/init/ModBlocks.java
+++ src/main/java/xyz/bpteam/mitrek/init/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 import xyz.bpteam.mitrek.MiTrek;
+import xyz.bpteam.mitrek.common.blocks.LightBlock;
 
 @Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.MOD)
 public class ModBlocks {
@@ -20,6 +21,8 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MiTrek.MODID);
     public static final RegistryObject<Block> DILITHIUM_BLOCK = BLOCKS.register("dilithium_block", () -> new Block(Block.Properties.create(Material.IRON)));
     public static final RegistryObject<Block> DILITHIUM_ORE = BLOCKS.register("dilithium_ore", () -> new Block(Block.Properties.create(Material.IRON)));
+    public static final RegistryObject<Block> LIGHT_BLOCK = BLOCKS.register("light_block", () -> new LightBlock(Block.Properties.create(Material.GLASS)));
+
 
     @SubscribeEvent
     public static void registerItemBlocks(final RegistryEvent.Register<Item> event) {
---
+++ src/main/java/xyz/bpteam/mitrek/init/ModItemGroups.java
@@ -0,0 +1,28 @@
+package xyz.bpteam.mitrek.init;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import xyz.bpteam.mitrek.MiTrek;
+
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
+/*public class ModItemGroups {
+
+    public static final ItemGroup MITREK_BLOCKS = new ModItemGroup(MiTrek.MODID), () ->
+
+    public static final class ModItemGroup extends ItemGroup {
+
+        @Nonnull
+        private final Supplier <ItemStack> iconSupplier;
+
+        public ModItemGroup(String label, Supplier<ItemStack> iconSupplier) {
+            super(label);
+        }
+
+        @Override
+        public ItemStack createIcon() {
+            return null;
+        }
+    }
+}*/
