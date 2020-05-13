4817c27e1c20967e282773f089a0816d126c5b11
UKMinecrafted <ukminecrafted@gmail.com>
2020-05-13 01:44:20 -0500

    started ore gen, dilithium gen, and dimension gen

---
+++ src/main/java/xyz/bpteam/mitrek/init/ModDimensions.java
@@ -0,0 +1,14 @@
+package xyz.bpteam.mitrek.init;
+
+import net.minecraft.world.dimension.Dimension;
+import net.minecraftforge.common.extensions.IForgeDimension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModDimensions {
+
+    public static final List<Dimension> DIMENSION_LIST = new ArrayList<>();
+
+
+}
---
+++ src/main/java/xyz/bpteam/mitrek/world/generation/OreGeneration.java
@@ -0,0 +1,27 @@
+package xyz.bpteam.mitrek.world.generation;
+
+import com.mojang.datafixers.Dynamic;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationSettings;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import xyz.bpteam.mitrek.init.ModBlocks;
+
+import java.util.Random;
+import java.util.function.Function;
+
+public class OreGeneration extends Feature<OreFeatureConfig> {
+
+
+    public OreGeneration(Function<Dynamic<?>, ? extends OreFeatureConfig> configFactoryIn) {
+        super(configFactoryIn);
+    }
+
+    @Override
+    public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, OreFeatureConfig config) {
+        return true;
+    }
+}
