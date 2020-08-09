c7851386cb35824974cc8b75720a013c8cbb9294
UKMinecrafted <ukminecrafted@gmail.com>
2020-08-08 21:03:51 -0500

    maybe?

--- src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
+++ src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
@@ -1,15 +1,46 @@
 package xyz.bpteam.mitrek.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 public class BlockShipConstructor extends BlockBase {
-    public BlockShipConstructor(Properties properties) {
-        super(properties);
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
+    public BlockShipConstructor(Block.Properties builder) {
+
+        super(builder);
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, Direction.NORTH));
     }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+    }
+
     int state = 0;
 
     public static final VoxelShape SHAPE = BlockBase.makeCuboidShape(16.0D, 0.0D, 16.0D, 0.0D, 8.0D, 0.0D);
@@ -21,4 +52,6 @@ public class BlockShipConstructor extends BlockBase {
     public boolean isOpaque() {
         return true;
     }
+
+
 }
