28e447f0b57716bc72473adb383dc587db59898e
UKMinecrafted <ukminecrafted@gmail.com>
2020-08-11 21:20:51 -0500

    fix to merge

--- src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
+++ src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
@@ -1,17 +1,17 @@
 package xyz.bpteam.mitrek.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 public class BlockShipConstructor extends BlockBase {
-<<<<<<< Updated upstream
-    public BlockShipConstructor(Properties properties) {
-        super(properties);
-    }
-=======
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
 
@@ -29,7 +29,6 @@ public class BlockShipConstructor extends BlockBase {
 
 
 
->>>>>>> Stashed changes
     int state = 0;
 
     public static final VoxelShape SHAPE = BlockBase.makeCuboidShape(16.0D, 0.0D, 16.0D, 0.0D, 8.0D, 0.0D);
