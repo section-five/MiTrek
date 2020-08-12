bebd72ff97f4bcf3e7e57380c415a8ade196baa4
UKMinecrafted <ukminecrafted@gmail.com>
2020-08-11 21:18:06 -0500

    POG Directional Blocks

--- src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
+++ src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
@@ -7,9 +7,29 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 public class BlockShipConstructor extends BlockBase {
+<<<<<<< Updated upstream
     public BlockShipConstructor(Properties properties) {
         super(properties);
     }
+=======
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
+
+    public BlockShipConstructor(Block.Properties builder) {
+        super(builder);
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+
+
+>>>>>>> Stashed changes
     int state = 0;
 
     public static final VoxelShape SHAPE = BlockBase.makeCuboidShape(16.0D, 0.0D, 16.0D, 0.0D, 8.0D, 0.0D);
