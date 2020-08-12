f1fa527f74a7c0e2fb8c8075beaec300b1bb77f5
UKMinecrafted <ukminecrafted@gmail.com>
2020-08-11 21:37:22 -0500

    open state

--- src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
+++ src/main/java/xyz/bpteam/mitrek/blocks/BlockShipConstructor.java
@@ -3,20 +3,36 @@ package xyz.bpteam.mitrek.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class BlockShipConstructor extends BlockBase {
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
 
 
     public BlockShipConstructor(Block.Properties builder) {
         super(builder);
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, Direction.NORTH).with(OPEN ,Boolean.valueOf(false)));
+    }
+
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+    state = state.cycle(OPEN);
+    worldIn.setBlockState(pos, state);
+    return ActionResultType.SUCCESS;
     }
 
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
