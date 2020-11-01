0e445e2460cc520f6b3619c48f377e241c81d1e7
UKMinecrafted <ukminecrafted@gmail.com>
2020-11-01 16:06:00 -0600

    dilithium variation

--- src/main/java/me/ukminecrafted/mitrek/blocks/BlockDilithiumCrystal.java
+++ src/main/java/me/ukminecrafted/mitrek/blocks/BlockDilithiumCrystal.java
@@ -1,7 +1,9 @@
 package me.ukminecrafted.mitrek.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.shapes.*;
 import net.minecraft.world.IBlockReader;
 
@@ -16,10 +18,15 @@ public class BlockDilithiumCrystal extends BlockBase {
     public static final VoxelShape SHAPE = makeCuboidShape(5.0D, 0.0D, 5.0D, 12.0D, 7.0D, 11.0D);
   
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SHAPE;
+        Vec3d vec3d = state.getOffset(worldIn, pos);
+        return SHAPE.withOffset(vec3d.x, vec3d.y, vec3d.z);
     }
 
     public boolean isOpaque() {
         return true;
+    }
+
+    public Block.OffsetType getOffsetType() {
+        return Block.OffsetType.XZ;
     }
 }
