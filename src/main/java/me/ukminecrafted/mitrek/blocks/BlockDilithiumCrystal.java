b0ebd194c75ad7d8a68a98e6fcce381ba96b5370
UKMinecrafted <ukminecrafted@gmail.com>
2021-02-05 12:57:34 -0600

    fixed crystal voxel shape
    mc removed vec3d?

--- src/main/java/me/ukminecrafted/mitrek/blocks/BlockDilithiumCrystal.java
+++ src/main/java/me/ukminecrafted/mitrek/blocks/BlockDilithiumCrystal.java
@@ -1,11 +1,13 @@
-/*package me.ukminecrafted.mitrek.blocks;
+package me.ukminecrafted.mitrek.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.shapes.*;
 import net.minecraft.world.IBlockReader;
+
+import java.util.function.ToIntFunction;
 
 public class BlockDilithiumCrystal extends BlockBase {
     public static final VoxelShape DILITHIUM_CRYSTAL = VoxelShapes.create(0.9375, 0.9375, 0.9375, 0.9, 1, 0.9);
@@ -14,12 +16,17 @@ public class BlockDilithiumCrystal extends BlockBase {
         super(properties);
     }
 
+    private static ToIntFunction<BlockState> getLightValueLit(int lightValue) {
+        return (state) -> {
+            return state.get(BlockStateProperties.LIT) ? lightValue : 15;
+        };
+    }
+
 
     public static final VoxelShape SHAPE = makeCuboidShape(5.0D, 0.0D, 5.0D, 12.0D, 7.0D, 11.0D);
   
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        Vec3d vec3d = state.getOffset(worldIn, pos);
-        return SHAPE.withOffset(vec3d.x, vec3d.y, vec3d.z);
+        return SHAPE;
     }
 
     public boolean isOpaque() {
@@ -29,4 +36,4 @@ public class BlockDilithiumCrystal extends BlockBase {
     public OffsetType getOffsetType() {
         return OffsetType.XZ;
     }
-}*/
+}
